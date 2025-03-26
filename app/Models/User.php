70d2767c284bcb31cdc447a5be10a0567cf7c1fe
Ramiruuu <oclaritremar25@gmail.com>
2025-03-26 18:40:52 +0800

    Fixes

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,7 +6,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 
     class User extends Model{
-        protected $table = 'tbluser1';
+        protected $table = 'tbluser2';
         // column sa table
         protected $fillable = ['username', 'password','gender'];
 
